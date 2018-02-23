8e46b1340a38dde386cc1095448f058cd798c856
robson-silverio <robsonsilveriosantos@gmail.com>
2018-02-23 15:41:36 -0300

    alteração app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { IesService } from './services/ies.service';
-
+import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { RoutingModule } from './app.router';
+
+
 
 import { AppComponent } from './app.component';
 
@@ -11,19 +11,20 @@ import { CursosComponent } from './cursos/cursos.component';
 import { DisciplinasComponent } from './disciplinas/disciplinas.component';
 import { IesComponent } from './ies/ies.component';
 
-//import { DisciplinaService } from './services/disciplina.service';
+import { IesService } from './services/ies.service';
+import { DisciplinaService } from './services/disciplina.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CursosComponent,
     DisciplinasComponent,
-    IesComponent,
-    //DisciplinaService
+    IesComponent
+    
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    RoutingModule    
   ],
   providers: [DisciplinaService, IesService],
   bootstrap: [AppComponent]
