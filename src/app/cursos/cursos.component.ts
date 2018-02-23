7b7dc677cc192b270e2f0eb4c223c7cb339e5aa4
Diego Sanchez <diegosilvasanchez@yahoo.com.br>
2018-02-23 17:55:24 -0300

    Inclusão de Paginação

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -9,12 +9,45 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   public cursos = [];
+  qtdPaginas = 0;
+  paginacao: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  paginacaoOffset = 0;
 
   constructor(
     private service: CursoService
   ) { }
 
   ngOnInit() {
+    this.contaPaginas();
     this.service.obtemListaCurso(0).subscribe(res => this.cursos = res);
+  }
+
+  contaPaginas() {
+    this.service.contaPaginas().subscribe(res => this.qtdPaginas = parseInt(res, 10));
+  }
+
+  paginado(pagina: number) {
+    this.service.obtemListaCurso(pagina).subscribe(res => this.cursos = res);
+  }
+
+  proximo() {
+    this.paginacaoOffset = this.paginacaoOffset + 10;
+
+    for (const num of this.paginacao) {
+      this.paginacao[num] = this.paginacaoOffset + num;
+    }
+    this.paginado(this.paginacao[0]);
+    console.log(this.paginacaoOffset);
+  }
+
+  anterior() {
+    if (this.paginacaoOffset > 0) {
+      this.paginacaoOffset = this.paginacaoOffset - 10;
+      for (const num of this.paginacao) {
+        this.paginacao[num] = this.paginacaoOffset + num;
+      }
+      this.paginado(this.paginacao[0]);
+    }
+    console.log(this.paginacaoOffset);
   }
 }  
--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -35,7 +35,7 @@ export class CursoService {
     return this.getServer(urlcursoLista.concat(pagina.toString()));
   }
 
-  contaPaginas(paginas:number) {
+  contaPaginas() {
     return this.getServer(urlPaginas);
   }
 }
