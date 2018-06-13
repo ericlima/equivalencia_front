99b3abd995bacc86df78c9866c64a0d37b1b6014
Eric Lima <eric.george.lima@gmail.com>
2018-06-13 13:38:15 -0300

    labs2

--- src/components/labs/labs.component.ts
+++ src/components/labs/labs.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabsComponent implements OnInit {
 
+  public prova = {
+    'titulo' : 'Prova A1: 20/05/2018 09:00',
+    'disciplinas' : [
+      {'nome' : 'disciplina1'},
+      {'nome' : 'disciplina2'},
+      {'nome' : 'disciplina3'}
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
